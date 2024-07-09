a1bb8bbc3c1727ba5b24b3a023ff36ac8e04cf0a
c0b23042 <c0b23042a1@edu.teu.ac.jp>
2024-07-09 17:38:52 +0900

    アイテムの種類と価値の追加

--- GTC.py
+++ GTC.py
@@ -63,9 +63,20 @@ class Item(pg.sprite.Sprite):
     出現する現金のクラス
     現金のグラフィックの違いや価値の違いは今後実装する
     """
+    imgs = [pg.image.load(f"fig/cash{i}.png") for i in range(1, 4)]
+
     def __init__(self):
         super().__init__()
-        self.image = pg.image.load(f"fig/cash.png")
+        self.luck = random.randint(1, 100)
+        if self.luck < 70:
+            self.image = __class__.imgs[2]
+            self.score = 1
+        elif self.luck < 90:
+            self.image = __class__.imgs[1]
+            self.score = 10
+        else:
+            self.image = __class__.imgs[0]
+            self.score = 20
         self.image = pg.transform.scale(self.image, (66, 66))
         self.rect = self.image.get_rect()
         self.rect.x = 800
@@ -76,7 +87,6 @@ class Item(pg.sprite.Sprite):
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
-
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
@@ -101,14 +111,14 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img_2,[-x+4800,0])
         key_lst = pg.key.get_pressed()
-        if tmr % 1000 == 0:
+        if tmr % 200 == 0:
             item = Item()
             items.add(item)
         if tmr % 500 == 0:
             emy = Enemy()
             emys.add(emy)
         for item in pg.sprite.spritecollide(car, items, True):
-            score += 10
+            score += item.score
         items.update()
         items.draw(screen)
         emys.update()
