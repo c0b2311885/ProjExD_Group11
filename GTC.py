0bf187c9635040f9a404c127494cb9705eadc7a9
C0B23118 <c0b2311885@edu.teu.ac.jp>
2024-07-16 13:59:44 +0900

    コメントを追加

--- GTC.py
+++ GTC.py
@@ -53,22 +53,22 @@ def main():
     bg_img_2 = pg.transform.flip(bg_img,True,False)
     ko_img = pg.image.load("fig/car.png")#車の描画
     ko_img = pg.transform.flip(ko_img,True,False)#車の画像の反転
-    ko_rect = ko_img.get_rect() #こうかとんのRect抽出
-    ko_rect.center = 300, 200
+    ko_rect = ko_img.get_rect() #車のRect抽出
+    ko_rect.center = 300, 200#中央を指定
     tmr = 0
     pg.mixer.music.load("fig\カーチェイス!!.mp3")  # BGMをロード
     pg.mixer.music.play(-1)  # BGMを再生
     while True:
-        for event in pg.event.get():
+        for event in pg.event.get():#イベントが起こった時の処理
             if event.type == pg.QUIT: return
-        x = tmr%3706
-        screen.blit(bg_img, [-x, 0])
+        x = tmr%3706#画面がループするようにする処理
+        screen.blit(bg_img, [-x, 0])#この一連のブリットで背景がループしても違和感がないようにする。
         screen.blit(bg_img_2,[-x+1853,0])
         screen.blit(bg_img, [-x+3706, 0])
         screen.blit(bg_img_2,[-x+5559,0])
-        key_lst = pg.key.get_pressed()
-        mx = 0
-        my = 0
+        key_lst = pg.key.get_pressed()#keyごとの処理を行うための下準備
+        mx = 0#x軸への移動量の初期化
+        my = 0#y軸への移動量の初期化
         if key_lst[pg.K_UP]:
             my = -1
         if key_lst[pg.K_DOWN]:
@@ -78,7 +78,7 @@ def main():
         if key_lst[pg.K_RIGHT]:
             mx = 1
         ko_rect.move_ip((mx,my))
-        screen.blit(ko_img, ko_rect) #こうかとんRectの貼り付け
+        screen.blit(ko_img, ko_rect) #車Rectの貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(200)
